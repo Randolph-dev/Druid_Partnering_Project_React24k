0b42d588b0e73430b2e73c0885ea2ba31e6c29bb
Alex Tran <tranduclong092@gmail.com>
2024-10-23 13:30:52 +0300

    SCRUM-41 Feat: add a simple button to test bootstrap application

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -1,9 +1,11 @@
+import { Button } from 'react-bootstrap';
 
 function App() {
 
   return (
     <>
       <h1>Hello world! This is team 3</h1>
+      <Button variant="primary">Click me!</Button>
     </>
   )
 }
