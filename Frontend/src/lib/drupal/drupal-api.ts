32a17c20ebd055fa875fc972e634a3e5016dacf4
Alex Tran <tranduclong092@gmail.com>
2024-10-24 14:28:30 +0300

    SCRUM-55 Refactor: export the types and change Links type name for more descriptive JsonApiLinks

--- Frontend/src/lib/drupal/drupal-api.ts
+++ Frontend/src/lib/drupal/drupal-api.ts
@@ -3,16 +3,17 @@ import axios from "axios";
 // This default URL should return everything exported by the JSON API
 const drupalUrl: string = "http://127.0.0.1:51604/api/";
 
-interface JsonApiLink {
+export interface JsonApiLink {
   href: string;
 }
 
-interface Links {
+export interface JsonApiLinks {
   [key: string]: JsonApiLink;
 }
 
-const fetchJsonApiLinksFromDrupal = async (): Promise<Links> => {
-  const { data: { links } } = await axios.get<{ links: Links }>(drupalUrl);
+const fetchJsonApiLinksFromDrupal = async (): Promise<JsonApiLinks> => {
+  const { data: { links } } = await axios.get<{ links: JsonApiLinks }>(drupalUrl);
+  console.log(links["node--article"]);
   return links;
 };
 
