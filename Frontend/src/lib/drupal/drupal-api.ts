ccdba057f9a580ed62a4fc0a63f6b89fafae855a
Alex Tran <tranduclong092@gmail.com>
2024-10-24 13:41:48 +0300

    SCRUM-55 Feat: type the response from Drupal api

--- Frontend/src/lib/drupal/drupal-api.ts
+++ Frontend/src/lib/drupal/drupal-api.ts
@@ -3,9 +3,52 @@ import axios from "axios";
 // this default url should return everything exported by json api
 const drupalUrl: string = "http://127.0.0.1:51604/api/";
 
-const fetchAllFromDrupal = async () => {
-  const res = await axios.get(drupalUrl);
-  console.log(res.data);
+interface JsonApiLink {
+  href: string;
 }
 
-export default fetchAllFromDrupal;
+interface MetaLinks {
+  self: JsonApiLink;
+}
+
+interface JsonApiMeta {
+  links: MetaLinks;
+}
+
+interface JsonApi {
+  version: string;
+  meta: JsonApiMeta;
+}
+
+interface MetaMe {
+  id: string;
+}
+
+interface MeLink {
+  meta: MetaMe;
+  href: string;
+}
+
+interface Meta {
+  links: {
+    me: MeLink;
+  };
+}
+
+interface Links {
+  [key: string]: JsonApiLink;
+}
+
+interface DrupalJsonApiResponse {
+  jsonapi: JsonApi;
+  data: any[];
+  meta: Meta;
+  links: Links;
+}
+
+const fetchJsonApiFromDrupal = async (): Promise<DrupalJsonApiResponse> => {
+  const res = await axios.get<DrupalJsonApiResponse>(drupalUrl);
+  return res.data;
+}
+
+export default fetchJsonApiFromDrupal;
