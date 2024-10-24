c8d36952863bd9ff0abb29120174c1875a639db5
Randolph <86254950+RR2894@users.noreply.github.com>
2024-10-24 10:57:15 +0300

    SCRUM-57 Fixed basic layout, removed unnecessary/duplicate Components

--- Frontend/src/pages/NavBar.tsx
+++ Frontend/src/pages/NavBar.tsx
@@ -1,3 +1,4 @@
+import { Link } from 'react-router-dom';
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
@@ -7,20 +8,20 @@ function NavBar() {
   return (
     <Navbar expand="lg" className="bg-body-tertiary">
       <Container>
-        <Navbar.Brand href="#home">Druid</Navbar.Brand>
+        <Navbar.Brand as={Link} to="/">Druid</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
-            <Nav.Link href="/">Home</Nav.Link>
-            <Nav.Link href="/">Page 1</Nav.Link>
+            <Nav.Link as={Link} to="/">Home</Nav.Link>
+            <Nav.Link as={Link} to="/about-us">About Us</Nav.Link>
+            <Nav.Link as={Link} to="/services">Services</Nav.Link>
+            <Nav.Link as={Link} to="/contact">Contact</Nav.Link>
             <NavDropdown title="Blog" id="basic-nav-dropdown">
-              <NavDropdown.Item href="#action/3.1">Blog 1</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.2">Blog 2</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.3">Blog 3</NavDropdown.Item>
+              <NavDropdown.Item as={Link} to="/blog/1">Blog 1</NavDropdown.Item>
+              <NavDropdown.Item as={Link} to="/blog/2">Blog 2</NavDropdown.Item>
+              <NavDropdown.Item as={Link} to="/blog/3">Blog 3</NavDropdown.Item>
               <NavDropdown.Divider />
-              <NavDropdown.Item href="#action/3.4">
-                Show all blog
-              </NavDropdown.Item>
+              <NavDropdown.Item as={Link} to="/blog">Show all blogs</NavDropdown.Item>
             </NavDropdown>
           </Nav>
         </Navbar.Collapse>
