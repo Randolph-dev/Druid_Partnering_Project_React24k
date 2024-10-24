5276668a1614259f56cd91585ee22149361e72f2
Randolph <86254950+RR2894@users.noreply.github.com>
2024-10-24 14:08:36 +0300

    SCRUM-56 Added Navbar following agreed project stylization

--- Frontend/src/pages/NavBar.tsx
+++ Frontend/src/pages/NavBar.tsx
@@ -1,30 +1,59 @@
+import { useState } from 'react';
 import { Link } from 'react-router-dom';
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
-import NavDropdown from 'react-bootstrap/NavDropdown';
+import Offcanvas from 'react-bootstrap/Offcanvas';
 
 function NavBar() {
+  const [show, setShow] = useState(false);
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+
   return (
-    <Navbar expand="lg" className="bg-body-tertiary">
+    <Navbar expand={false} className="bg-body-tertiary">
       <Container>
         <Navbar.Brand as={Link} to="/">Druid</Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="me-auto">
-            <Nav.Link as={Link} to="/">Home</Nav.Link>
-            <Nav.Link as={Link} to="/about-us">About Us</Nav.Link>
-            <Nav.Link as={Link} to="/services">Services</Nav.Link>
-            <Nav.Link as={Link} to="/contact">Contact</Nav.Link>
-            <NavDropdown title="Blog" id="basic-nav-dropdown">
-              <NavDropdown.Item as={Link} to="/blog/1">Blog 1</NavDropdown.Item>
-              <NavDropdown.Item as={Link} to="/blog/2">Blog 2</NavDropdown.Item>
-              <NavDropdown.Item as={Link} to="/blog/3">Blog 3</NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item as={Link} to="/blog">Show all blogs</NavDropdown.Item>
-            </NavDropdown>
-          </Nav>
-        </Navbar.Collapse>
+
+        <Navbar.Toggle aria-controls="offcanvasNavbar" onClick={handleShow} />
+
+        <Navbar.Offcanvas
+          id="offcanvasNavbar"
+          aria-labelledby="offcanvasNavbarLabel"
+          placement="end"
+          show={show}
+          onHide={handleClose}
+          className="bg-dark text-light"
+        >
+          {/* Adjusted X button position */}
+          <Offcanvas.Header className="bg-dark text-light">
+            {/* Adjusted the position of the close button */}
+            <button 
+              type="button" 
+              className="btn-close btn-close-white" 
+              aria-label="Close" 
+              onClick={handleClose} 
+              style={{
+                position: 'absolute',
+                top: '10%',  
+                right: '4rem',  
+              }}
+            ></button>
+          </Offcanvas.Header>
+
+          <Offcanvas.Body className="d-flex flex-column justify-content-center">
+            <Nav className="flex-column text-center">
+              <Nav.Link as={Link} to="/about-us" onClick={handleClose} className="text-light mb-3" style={{ fontSize: '26px' }}>About Us</Nav.Link>
+              <Nav.Link as={Link} to="/cases" onClick={handleClose} className="text-light mb-3" style={{ fontSize: '26px' }}>Cases</Nav.Link>
+              <Nav.Link as={Link} to="/services" onClick={handleClose} className="text-light mb-3" style={{ fontSize: '26px' }}>Services</Nav.Link>
+              <Nav.Link as={Link} to="/workingwithus" onClick={handleClose} className="text-light mb-3" style={{ fontSize: '26px' }}>Working with us</Nav.Link>
+              <Nav.Link as={Link} to="/contact" onClick={handleClose} className="text-light mb-3" style={{ fontSize: '26px' }}>Contact</Nav.Link>
+              <Nav.Link as={Link} to="/blog" onClick={handleClose} className="text-light mb-3" style={{ fontSize: '26px' }}>Blog</Nav.Link>
+              <Nav.Link as={Link} to="#" onClick={handleClose} className="text-light mb-3" style={{ fontSize: '26px' }}>english | suomi</Nav.Link>
+            </Nav>
+          </Offcanvas.Body>
+        </Navbar.Offcanvas>
       </Container>
     </Navbar>
   );
