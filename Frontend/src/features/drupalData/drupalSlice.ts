0bec138b0a93cc4f584da871351c23497101abb1
Alex Tran <tranduclong092@gmail.com>
2024-10-25 11:11:21 +0300

    SCRUM-51 Feat: create a reducer to fetch api links from drupal and store in state

--- Frontend/src/features/drupalData/drupalSlice.ts
+++ Frontend/src/features/drupalData/drupalSlice.ts
@@ -1,21 +1,34 @@
-import { createSlice } from "@reduxjs/toolkit"
+import { createSlice, PayloadAction } from "@reduxjs/toolkit"
+import fetchJsonApiLinksFromDrupal, { JsonApiLinks } from "../../lib/drupal/drupal-api";
 
 
 export interface DrupalState {
-  value: number
+  value: number,
+  jsonApiLinks: JsonApiLinks,
 }
 
 const initialState: DrupalState = {
   value: 0,
+  jsonApiLinks: {} as JsonApiLinks
 }
 
 export const drupalSlice = createSlice({
   name: 'drupal',
   initialState,
   reducers: {
+    // just a place holder reducer, doesn't actually do anything
     increment: (state) => {
       state.value += 1;
     },
+  },
+  // this extra reducer call the api function to get api links and store it in state
+  extraReducers: (builder) => {
+    builder.addCase(
+      fetchJsonApiLinksFromDrupal.fulfilled,
+      (state, action: PayloadAction<JsonApiLinks>) => {
+        state.jsonApiLinks = action.payload;
+      },
+    );
   },
 });
 
