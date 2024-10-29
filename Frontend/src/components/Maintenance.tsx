98fc4bf29498c5477e4e23bec57722c81d4531fb
Alex Tran <tranduclong092@gmail.com>
2024-10-29 14:58:10 +0200

    SCRUM-59 Feat: fetch data for Maintenance page

--- Frontend/src/components/Maintenance.tsx
+++ Frontend/src/components/Maintenance.tsx
@@ -1,6 +1,16 @@
-
+import { useEffect } from 'react';
+import fetchContentFromDrupal from '../lib/drupal/drupal-content-api';
+import { useAppSelector } from '../hooks/hooks';
 
 export default function Maintenance() {
+  const jsonApiLinks = useAppSelector(state => state.drupal.jsonApiLinks);
+
+  useEffect(() => {
+    // the link node--page will return all 3 pages of type "service pages", we just want the second one which is Maintenance
+    fetchContentFromDrupal(jsonApiLinks["node--service"])
+      .then(res => console.log(res.data[1]))
+  }, [jsonApiLinks])
+
   return (
     <div>Maintenance</div>
   )
