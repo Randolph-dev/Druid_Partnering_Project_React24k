1b06aff2e992273b83143a0f7171dab6985f0a3e
Alex Tran <tranduclong092@gmail.com>
2024-10-29 14:16:30 +0200

    SCRUM-59 Feat: fetch content for Jobs from Drupal

--- Frontend/src/components/Jobs.tsx
+++ Frontend/src/components/Jobs.tsx
@@ -1,5 +1,14 @@
+import { useEffect } from 'react';
+import fetchContentFromDrupal from '../lib/drupal/drupal-content-api';
+import { useAppSelector } from '../hooks/hooks';
+
 
 export default function Jobs() {
+  const jsonApiLinks = useAppSelector(state => state.drupal.jsonApiLinks);
+  useEffect(() => {
+    console.log(fetchContentFromDrupal(jsonApiLinks["node--page"]));
+  }, [jsonApiLinks])
+
   return (
     <div>Jobs</div>
   )
