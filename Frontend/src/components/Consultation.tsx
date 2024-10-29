0ebc9a8d0615df7e3a45091ac221854562c17e0a
Alex Tran <tranduclong092@gmail.com>
2024-10-29 14:53:42 +0200

    SCRUM-59 Feat: fetch data for consultation page

--- Frontend/src/components/Consultation.tsx
+++ Frontend/src/components/Consultation.tsx
@@ -1,6 +1,16 @@
-
+import { useEffect } from 'react';
+import fetchContentFromDrupal from '../lib/drupal/drupal-content-api';
+import { useAppSelector } from '../hooks/hooks';
 
 export default function Consultation() {
+  const jsonApiLinks = useAppSelector(state => state.drupal.jsonApiLinks);
+
+  useEffect(() => {
+    // the link node--page will return all 3 pages of type "service pages", we just want the last one which is consultation
+    fetchContentFromDrupal(jsonApiLinks["node--service"])
+      .then(res => console.log(res.data[2]))
+  }, [jsonApiLinks])
+
   return (
     <div>Consultation</div>
   )
