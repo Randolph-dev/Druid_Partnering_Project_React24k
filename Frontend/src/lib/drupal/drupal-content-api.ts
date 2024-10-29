5e48eed109ace7675fbc12635c222b4270dc5d15
Alex Tran <tranduclong092@gmail.com>
2024-10-29 13:23:00 +0200

    SCRUM-59 Feat: destructure the axios response to keep only attributes field

--- Frontend/src/lib/drupal/drupal-content-api.ts
+++ Frontend/src/lib/drupal/drupal-content-api.ts
@@ -1,27 +1,33 @@
 import axios from "axios";
 import { JsonApiLink, JsonApiLinks } from "./drupal-api";
 
-export interface JsonApiData {
-  attributes: {
-    [key: string]: any; // accept any field of any type
-  };
+export interface JsonApiDataAttributes {
+  [key: string]: any; // Accept any field of any type
 }
 
-// response structure contains content data and links
+// response structure contains content attributes and links
 export interface JsonApiContent {
-  data: JsonApiData[];
+  data: JsonApiDataAttributes[];
   links: JsonApiLinks;
 }
 
 // Fetch content from a Json API link, destructure the data/attribute and links
 const fetchContentFromDrupal = async (link: JsonApiLink): Promise<JsonApiContent> => {
-  const { data: { data, links } } = await axios.get<JsonApiContent>(link.href);
+
+  // destructure the response in drupal and keep only the attributes fields and the links
+  const { data: { data: data, links } } = await axios.get<{
+    data: { attributes: JsonApiDataAttributes }[],
+    links: JsonApiLinks
+  }>(link.href);
+
+  // further destructuring the data object to keep only the attributes
+  const attributesOnly = data.map((item) => item.attributes);
 
   // Log the attributes and links for each content item
-  console.log("Data:", data);
+  console.log("Data:", attributesOnly);
   console.log("Links:", links);
 
-  return { data, links };
+  return { data: attributesOnly, links };
 }
 
 export default fetchContentFromDrupal;
