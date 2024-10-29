a95d381d2231bd6831c87cb02b0ba4e168a519d1
Alex Tran <tranduclong092@gmail.com>
2024-10-29 13:48:26 +0200

    SCRUM-59 Feat: add useEffect block to fetch data for homepage

--- Frontend/src/components/Home.tsx
+++ Frontend/src/components/Home.tsx
@@ -1,7 +1,16 @@
 import { Button } from 'react-bootstrap';
 import { Container } from 'react-bootstrap';
+import { useAppSelector } from '../hooks/hooks';
+import { useEffect } from 'react';
+import fetchContentFromDrupal from '../lib/drupal/drupal-content-api';
 
 export default function Home() {
+  const jsonApiLinks = useAppSelector(state => state.drupal.jsonApiLinks);
+
+  useEffect(() => {
+    console.log(fetchContentFromDrupal(jsonApiLinks["node--front_page"]));
+  }, [jsonApiLinks])
+
   return (
     <Container fluid className="p-5">
       <h1>Hello world! This is team 3</h1>
