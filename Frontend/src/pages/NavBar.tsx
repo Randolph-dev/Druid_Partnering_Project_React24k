f56e057314653cf1d6c3a0d610dc370938ba8553
Alex Tran <tranduclong092@gmail.com>
2024-10-29 13:59:13 +0200

    SCRUM-59 Feat: add a dropdown menu for the navbar

--- Frontend/src/pages/NavBar.tsx
+++ Frontend/src/pages/NavBar.tsx
@@ -4,6 +4,7 @@ import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import Offcanvas from 'react-bootstrap/Offcanvas';
+import { NavDropdown } from 'react-bootstrap';
 
 const NavBar: React.FC = () => {
   const [show, setShow] = useState<boolean>(false);
@@ -27,21 +28,30 @@ const NavBar: React.FC = () => {
           className="bg-dark text-light"
         >
           <Offcanvas.Header className="bg-dark text-light">
-            <button 
-              type="button" 
-              className="btn-close btn-close-white" 
-              aria-label="Close" 
-              onClick={handleClose} 
+            <button
+              type="button"
+              className="btn-close btn-close-white"
+              aria-label="Close"
+              onClick={handleClose}
               style={{
                 position: 'absolute',
-                top: '10%',  
-                right: '4rem',  
+                top: '10%',
+                right: '4rem',
               }}
             ></button>
           </Offcanvas.Header>
 
           <Offcanvas.Body className="d-flex flex-column justify-content-center">
-            <Nav className="flex-column text-center">
+            <Nav className="flex-column text-center text-light mb-3" style={{ fontSize: '26px' }}>
+              {/* this is a dropdown list for service pages including: projects, maintenance and consultation */}
+              {/* TODO: style this */}
+              <NavDropdown title="Services" id="basic-nav-dropdown">
+                <NavDropdown.Item href="/projects">Projects</NavDropdown.Item>
+                <NavDropdown.Divider />
+                <NavDropdown.Item href="#action/3.4">Maintenance</NavDropdown.Item>
+                <NavDropdown.Divider />
+                <NavDropdown.Item href="#action/3.4">Consultation</NavDropdown.Item>
+              </NavDropdown>
               <Nav.Link as={Link} to="/about-us" onClick={handleClose} className="text-light mb-3" style={{ fontSize: '26px' }}>About Us</Nav.Link>
               <Nav.Link as={Link} to="/cases" onClick={handleClose} className="text-light mb-3" style={{ fontSize: '26px' }}>Cases</Nav.Link>
               <Nav.Link as={Link} to="/services" onClick={handleClose} className="text-light mb-3" style={{ fontSize: '26px' }}>Services</Nav.Link>
