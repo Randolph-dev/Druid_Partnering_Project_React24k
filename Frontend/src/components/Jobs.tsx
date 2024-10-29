96a3ab0aded80913a52e9500ae32222d58a7957d
Alex Tran <tranduclong092@gmail.com>
2024-10-29 14:36:58 +0200

    SCRUM-59 Refactor: fetch only the data for Jobs page

--- Frontend/src/components/Jobs.tsx
+++ Frontend/src/components/Jobs.tsx
@@ -6,7 +6,9 @@ import { useAppSelector } from '../hooks/hooks';
 export default function Jobs() {
   const jsonApiLinks = useAppSelector(state => state.drupal.jsonApiLinks);
   useEffect(() => {
-    console.log(fetchContentFromDrupal(jsonApiLinks["node--page"]));
+    // the link node--page will return all 3 pages of type "basic pages", we just want the last one which is Jobs
+    fetchContentFromDrupal(jsonApiLinks["node--page"])
+      .then(res => console.log(res.data[2]))
   }, [jsonApiLinks])
 
   return (
