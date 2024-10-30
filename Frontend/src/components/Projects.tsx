af913b6882844aaf121b0eb00960883367c868ff
Alex Tran <tranduclong092@gmail.com>
2024-10-30 15:35:18 +0200

    SCRUM-87 Feat: add a fetch function to get all projects content from Drupal

--- Frontend/src/components/Projects.tsx
+++ Frontend/src/components/Projects.tsx
@@ -8,7 +8,10 @@ export default function Projects() {
   useEffect(() => {
     // the link node--page will return all 3 pages of type "service pages", we just want the first one which is Projects
     fetchContentFromDrupal(jsonApiLinks["node--service"])
-      .then(res => console.log(res.data[0]))
+      .then(res => console.log(res.data[0]));
+    // fetch all projects from node--projects
+    fetchContentFromDrupal(jsonApiLinks["node--projects"])
+      .then(res => console.log(res.data));
   }, [jsonApiLinks])
 
   return (
