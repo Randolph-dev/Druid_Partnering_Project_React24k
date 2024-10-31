e700bd97646982c3753f4a097849fa2471fef1e0
Alex Tran <tranduclong092@gmail.com>
2024-10-31 10:33:43 +0200

    SCRUM-74 Feat: write an async function to fetch, store and display homepage data

--- Frontend/src/components/Home.tsx
+++ Frontend/src/components/Home.tsx
@@ -1,20 +1,30 @@
 import { Button } from 'react-bootstrap';
 import { Container } from 'react-bootstrap';
 import { useAppSelector } from '../hooks/hooks';
-import { useEffect } from 'react';
-import fetchContentFromDrupal from '../lib/drupal/drupal-content-api';
+import { useEffect, useState } from 'react';
+import fetchContentFromDrupal, { JsonApiDataAttributes } from '../lib/drupal/drupal-content-api';
 
 export default function Home() {
   const jsonApiLinks = useAppSelector(state => state.drupal.jsonApiLinks);
+  const jsonApiLinksLoading = useAppSelector(state => state.drupal.isLoading);
+  const [homePageData, setHomePageData] = useState<JsonApiDataAttributes | null>(null);
 
   useEffect(() => {
-    console.log(fetchContentFromDrupal(jsonApiLinks["node--front_page"]));
-  }, [jsonApiLinks])
+    // Ensure loading is complete and jsonApiLinks is defined
+    if (!jsonApiLinksLoading) {
+      const fetchData = async () => {
+        const data = await fetchContentFromDrupal(jsonApiLinks["node--front_page"]);
+        setHomePageData(data);
+      };
+      fetchData();
+    }
+  }, [jsonApiLinks, jsonApiLinksLoading]);
 
   return (
     <Container fluid className="p-5">
       <h1>Hello world! This is team 3</h1>
       <Button variant="primary">Click me!</Button>
+      {homePageData && <div>{JSON.stringify(homePageData)}</div>}
     </Container>
   )
 }
