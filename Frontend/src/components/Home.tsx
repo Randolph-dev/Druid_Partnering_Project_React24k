c0944db590641271f4e336ae7a1822e43ed9c089
Nargis <nargissamatova16@gmail.com>
2024-10-31 12:58:05 +0200

    Add dummy text to header

--- Frontend/src/components/Home.tsx
+++ Frontend/src/components/Home.tsx
@@ -1,4 +1,4 @@
-import { Container } from 'react-bootstrap';
+import { Col, Container, Row } from 'react-bootstrap';
 import { useAppSelector } from '../hooks/hooks';
 import { useEffect, useState } from 'react';
 import fetchContentFromDrupal, { JsonApiDataAttributes } from '../lib/drupal/drupal-content-api';
@@ -23,17 +23,25 @@ export default function Home() {
     return <p>Loading</p>
   }
 
-  // console.log(homePageData);
+  console.log(homePageData);
 
   const { title, body, field_image_url: images } = homePageData;
 
   return (
     <Container fluid className="p-5">
       <h1>{title}</h1>
-      {body.value}
-      {images.map((image: { "uri": string, "title": string }, index: number) =>
+
+<Container>
+  <Row>
+    <Col lg={6}><p className='h2 fw-light'>We are a team of developers with a commitment to providing user-friendly, innovative, and reliable digital solutions. Our mission is to incoporate technology to businesses in order to help them bring their ideas to life.</p></Col>
+  </Row>
+</Container>
+
+
+      {/* body.value */}
+      {/* images.map((image: { "uri": string, "title": string }, index: number) =>
         <img key={`${index}_${image.title}`} src={image.uri} alt={image.title} />
-      )}
+  ) */}
     </Container>
   )
 }
