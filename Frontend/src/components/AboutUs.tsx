56376fc981001d247e1bba25fd0a0d2252c805bf
Randolph <86254950+RR2894@users.noreply.github.com>
2024-11-04 18:28:04 +0200

    added basic About Us layout - heading, paragraph, and image_url

--- Frontend/src/components/AboutUs.tsx
+++ Frontend/src/components/AboutUs.tsx
@@ -1,21 +1,51 @@
-import { useEffect } from 'react';
-import fetchContentFromDrupal from '../lib/drupal/drupal-content-api';
+import { useEffect, useState } from 'react';
+import fetchContentFromDrupal, {
+    JsonApiDataAttributes,
+} from "../lib/drupal/drupal-content-api";
 import { useAppSelector } from '../hooks/hooks';
+import { Container, Row, Col, Image } from 'react-bootstrap';
 
 const AboutUs: React.FC = () => {
     const jsonApiLinks = useAppSelector(state => state.drupal.jsonApiLinks);
+    const [aboutUsData, setAboutUsData] = useState<JsonApiDataAttributes | null>(null);
 
     useEffect(() => {
         // the link node--page will return all 3 pages of type "basic pages", we just want the second one which is About us
-        fetchContentFromDrupal(jsonApiLinks["node--page"])
-            .then(res => console.log(res.data[1]))
-    }, [jsonApiLinks])
+        if (jsonApiLinks["node--page"]) {
+            fetchContentFromDrupal(jsonApiLinks["node--page"])
+                .then(res => {
+                    console.log(res.data[1]); 
+                    setAboutUsData(res.data[1]);
+                });
+        }
+    }, [jsonApiLinks]);
+
+    if (!aboutUsData) {
+        return <div>Loading...</div>;
+    }
+
+    const {
+        field_heading,
+        field_paragraph,
+        field_image_url,
+    } = aboutUsData;
 
     return (
-        <div>
-            <h1>About us</h1>
-            <p>About us text will be here</p>
-        </div>
+        <Container className="p-4">           
+            <Container className="text-center my-4 p-2">
+                <h2>{field_heading[0]}</h2>
+            </Container>
+            <Row>
+                <Col md={6} className='p-4'>
+                    <Image src={field_image_url[0].uri} alt={field_image_url[0].title} fluid />
+                </Col>
+                <Col md={6} className="d-flex justify-content-end p-4" >
+                    <div className="text-start">
+                        <h2>{field_paragraph[0]}</h2>
+                    </div>
+                </Col>
+            </Row>
+        </Container>
     )
 }
 
