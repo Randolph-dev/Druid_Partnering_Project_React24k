d94ff40f602bd008ae1b064a6868d1831e5dc8cb
Alex Tran <tranduclong092@gmail.com>
2024-11-07 16:07:42 +0200

    SCRUM-128 Feat: add a function to change current page title

--- Frontend/src/pages/Layout.tsx
+++ Frontend/src/pages/Layout.tsx
@@ -1,9 +1,23 @@
 import { Container } from "react-bootstrap";
 import NavBar from "./NavBar";
-import { Outlet } from "react-router-dom";
+import { Outlet, useLocation } from "react-router-dom";
 import Footer from "./Footer";
+import { useEffect } from "react";
 
 export default function Layout() {
+  const location = useLocation();
+  const pathname = location.pathname;
+  let pageTitle: string;
+  if (pathname === "/") {
+    pageTitle = "Drupal websites and uncompromising software solutions | Druid";
+  } else {
+    pageTitle = `${location.pathname.slice(1)} | Drupal websites and uncompromising software solutions | Druid`
+  }
+
+  useEffect(() => {
+    document.title = pageTitle; // Set the desired page title
+  }, [location]);
+
   return (
     // page container
     <Container
