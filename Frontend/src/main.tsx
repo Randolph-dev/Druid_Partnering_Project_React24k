43aa05ea5f03d5758c677ed6673853dcd300aa25
Alex Tran <tranduclong092@gmail.com>
2024-11-07 15:47:39 +0200

    SCRUM-128 Change the default font to Euclid circular B

--- Frontend/src/main.tsx
+++ Frontend/src/main.tsx
@@ -4,6 +4,7 @@ import App from './App.tsx'
 import { Provider } from 'react-redux'
 import { store } from './store/store';
 import MauticTrackingScript from './lib/mautic/MauticTrackingScript.ts';
+import "./index.css";
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
