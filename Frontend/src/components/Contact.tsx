efa9121473113fe478630ba626c39c850eb9d80a
Alex Tran <tranduclong092@gmail.com>
2024-11-07 10:23:43 +0200

    SCRUM-123: type the response and render the header

--- Frontend/src/components/Contact.tsx
+++ Frontend/src/components/Contact.tsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from 'react';
 import fetchContentFromDrupal, { JsonApiDataAttributes } from '../lib/drupal/drupal-content-api';
 import { useAppSelector } from '../hooks/hooks';
 import ContactForm from './ContactForm';
+import { Container } from 'react-bootstrap';
 
 const Contact: React.FC = () => {
     const jsonApiLinks = useAppSelector(state => state.drupal.jsonApiLinks);
@@ -26,12 +27,32 @@ const Contact: React.FC = () => {
 
     console.log(contactPageData);
 
+    const {
+        field_first_section_header,
+    } = contactPageData;
+
+    type ContactPageField = {
+        value: string;
+        format: string;
+        processed: string;
+    };
+
     return (
-        <div>
-            <h1>Contact Us</h1>
-            <p>Contact page will be here</p>
+        <Container fluid className='p-0'>
+            <Container fluid className='p-5'>
+                <h1 className='my-5 d-flex gap-2'>
+                    {field_first_section_header.map((span: ContactPageField, index: number) => (
+                        <p key={index} dangerouslySetInnerHTML={{ __html: span.processed }} />
+                    ))}
+                </h1>
+                <p>Contact page will be here</p>
+            </Container>
+            <Container fluid className='p-5'>
+                <h1>Contact Us</h1>
+                <p>Contact page will be here</p>
+            </Container>
             <ContactForm />
-        </div>
+        </Container>
     )
 }
 
