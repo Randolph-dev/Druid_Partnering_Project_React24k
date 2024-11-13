7ff3b8af4804d2c1db1575687f13a44221bc0036
Alex Tran <tranduclong092@gmail.com>
2024-11-13 13:07:30 +0200

    SCRUM-134 Refactor: passing props and render Hero section

--- Frontend/src/components/HomePage/Hero.tsx
+++ Frontend/src/components/HomePage/Hero.tsx
@@ -1,6 +1,39 @@
+import { Col, Container, Row } from "react-bootstrap";
 
-export default function Hero() {
+type Field = {
+  value: string;
+  format: string;
+};
+
+export interface HeroProps {
+  section: {
+    field_frontpage_hero_intro_parag: Field[];
+  };
+}
+
+export default function Hero(props: HeroProps) {
+  const { section: { field_frontpage_hero_intro_parag: fields } } = props;
+
+  if (!fields) {
+    return <p>Loading</p>;
+  };
+
   return (
-    <div>Hero</div>
+    <Container
+      className="d-flex align-items-center"
+      style={{ height: "80vh" }}
+    >
+      <Row>
+        <Col lg={6}>
+          {fields.map((field, index) =>
+            <p
+              key={`${index}_${field.value.slice(0, 10)}`}
+              className="h1 fw-light"
+              dangerouslySetInnerHTML={{ __html: fields[0].value }}
+            ></p>
+          )}
+        </Col>
+      </Row>
+    </Container>
   )
 }
