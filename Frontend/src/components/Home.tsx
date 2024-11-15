7d1135db70fd2dc5adb47cef149d0b3623e0713c
Alex Tran <tranduclong092@gmail.com>
2024-11-15 11:42:00 +0200

    SCRUM-134 Feat: Add Redux state management for page data

--- Frontend/src/components/Home.tsx
+++ Frontend/src/components/Home.tsx
@@ -6,16 +6,23 @@ import Service from "./HomePage/Service";
 import Advertise from "./HomePage/Advertise";
 import { Paragraph, RestResponseData } from "../types/drupal";
 import Projects from "./HomePage/Projects";
+import { setPageData } from "../features/drupalData/drupalSlice";
+import { useDispatch, useSelector } from "react-redux";
+import { RootState } from "../store/store";
 
 export default function Home() {
-  const [frontPageData, setFrontPageData] = useState<Paragraph[] | []>([]); // TODO: type this properly
+  const drupalUrl: string = import.meta.env.VITE_DRUPAL_URL;
+  const dispatch = useDispatch();
+  const frontPageData = useSelector((state: RootState) => state.drupal.pageData.frontpage);
 
   useEffect(() => {
     const fetchData = async () => {
-      const { data } = await axios.get<RestResponseData[]>('https://druidpartneringapp.lndo.site/api/frontpage');
-      setFrontPageData(data[0]["field_frontpage_sections"]);
+      const { data } = await axios.get<RestResponseData[]>(`${drupalUrl}frontpage`);
+      dispatch(setPageData({ page: "frontpage", data: data[0]["field_frontpage_sections"] }));
     };
-    fetchData();
+    if (!frontPageData) {
+      fetchData()
+    }
   }, []);
 
   // console.log(frontPageData);
@@ -42,53 +49,11 @@ export default function Home() {
         }
       })}
 
-      {/* <Container className="my-5">
-        <Row className="justify-content-between">
-          <Col md={5}>
-            {images
-              .slice(1, -1)
-              .map((image: { uri: string; title: string }, index: number) => (
-                <div key={index} className="my-5">
-                  <Image src={image.uri} alt={image.title} fluid />
-                  <h3 className="my-4">City of Helsinki</h3>
-                  <p>
-                    New universal Drupal platform and the revamp of Hel.fi web
-                    service - efficient and accessible content production.
-                  </p>
-                  <p>&rarr; Read more</p>
-                </div>
-              ))}
-          </Col>
-
-          <Col md={6}>
-            <h1 className="d-flex justify-content-end">Our Work</h1>
-            <div>
-              {images.length > 0 && (
-                <div className="my-5">
-                  <Image
-                    src={images[images.length - 1].uri}
-                    alt={images[images.length - 1].title}
-                    fluid
-                  />
-                  <h3 className="my-4">
-                    Veikkaus {images[images.length - 1].title}
-                  </h3>
-                  <p>
-                    New universal Drupal platform and the revamp of Hel.fi web
-                    service - efficient and accessible content production.
-                  </p>
-                  <p>&rarr; Read more</p>
-                </div>
-              )}
-            </div>
-          </Col>
-        </Row>
-      </Container> */}
-
-      {/* <Container className="my-5">
+      {/*  */}
+      <Container className="my-5">
         <h1 className="text-center">Community for developers by developers.</h1>
         <p className="text-center">&rarr; Get to know our culture and people</p>
-      </Container> */}
+      </Container>
     </Container>
   );
 }
--- Frontend/src/features/drupalData/drupalSlice.ts
+++ Frontend/src/features/drupalData/drupalSlice.ts
@@ -1,15 +1,18 @@
 import { createSlice, PayloadAction } from "@reduxjs/toolkit"
 import fetchJsonApiLinksFromDrupal, { JsonApiLinks } from "../../lib/drupal/drupal-api";
+import { Paragraph } from "../../types/drupal";
 
 
 export interface DrupalState {
   isLoading: boolean,
   jsonApiLinks: JsonApiLinks,
+  pageData: Record<string, Paragraph[]>,
 }
 
 const initialState: DrupalState = {
   isLoading: true,
-  jsonApiLinks: {} as JsonApiLinks
+  jsonApiLinks: {} as JsonApiLinks,
+  pageData: {},
 }
 
 export const drupalSlice = createSlice({
@@ -20,6 +23,10 @@ export const drupalSlice = createSlice({
     setLoading: (state, action) => {
       state.isLoading = action.payload;
     },
+    setPageData: (state, action: PayloadAction<{ page: string, data: Paragraph[] }>) => {
+      const { page, data } = action.payload;
+      state.pageData[page] = data;
+    }
   },
   // this extra reducer call the api function to get api links and store it in state
   extraReducers: (builder) => {
@@ -32,6 +39,6 @@ export const drupalSlice = createSlice({
   },
 });
 
-export const { setLoading } = drupalSlice.actions;
+export const { setLoading, setPageData } = drupalSlice.actions;
 
 export default drupalSlice.reducer;
