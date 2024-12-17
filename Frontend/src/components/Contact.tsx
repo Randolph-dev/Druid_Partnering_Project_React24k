98ce574cd52a63d5c000a2e84c31f3ab98039072
Nargis <nargissamatova16@gmail.com>
2024-12-17 21:31:40 +0200

    Add info section and adjust the layout

--- Frontend/src/components/Contact.tsx
+++ Frontend/src/components/Contact.tsx
@@ -1,81 +1,149 @@
-import { useEffect, useState } from 'react';
-import fetchContentFromDrupal, { JsonApiDataAttributes } from '../lib/drupal/drupal-content-api';
-import { useAppSelector } from '../hooks/hooks';
-import ContactForm from './ContactForm';
-import { Container } from 'react-bootstrap';
-import ContactPageTeam from './ContactPageTeam';
+import { useEffect, useState } from "react";
+import fetchContentFromDrupal, {
+  JsonApiDataAttributes,
+} from "../lib/drupal/drupal-content-api";
+import { useAppSelector } from "../hooks/hooks";
+import ContactForm from "./ContactForm";
+import { Col, Container, Row } from "react-bootstrap";
+import ContactPageTeam from "./ContactPageTeam";
 
 const Contact: React.FC = () => {
-    const jsonApiLinks = useAppSelector(state => state.drupal.jsonApiLinks);
-    const jsonApiLinksLoading = useAppSelector((state) => state.drupal.isLoading);
-    const [contactPageData, setContactPageData] = useState<JsonApiDataAttributes | null>(null);
-    const [teamMembersData, setTeamMembersData] = useState<JsonApiDataAttributes | null>(null);
+  const jsonApiLinks = useAppSelector((state) => state.drupal.jsonApiLinks);
+  const jsonApiLinksLoading = useAppSelector((state) => state.drupal.isLoading);
+  const [contactPageData, setContactPageData] =
+    useState<JsonApiDataAttributes | null>(null);
+  const [teamMembersData, setTeamMembersData] =
+    useState<JsonApiDataAttributes | null>(null);
 
-    useEffect(() => {
-        if (!jsonApiLinksLoading) {
-            const fetchData = async () => {
-                const pageData = await fetchContentFromDrupal(
-                    jsonApiLinks["node--contact_page"]
-                );
-                setContactPageData(pageData.data[0]);
-                const teamData = await fetchContentFromDrupal(
-                    jsonApiLinks["node--team_member"]
-                );
-                setTeamMembersData(teamData.data)
-            };
-            fetchData();
-        }
-    }, [jsonApiLinksLoading]);
-
-    if (!contactPageData) {
-        return <p>Loading</p>;
+  useEffect(() => {
+    if (!jsonApiLinksLoading) {
+      const fetchData = async () => {
+        const pageData = await fetchContentFromDrupal(
+          jsonApiLinks["node--contact_page"]
+        );
+        setContactPageData(pageData.data[0]);
+        const teamData = await fetchContentFromDrupal(
+          jsonApiLinks["node--team_member"]
+        );
+        setTeamMembersData(teamData.data);
+      };
+      fetchData();
     }
+  }, [jsonApiLinksLoading]);
 
-    const {
-        field_first_section_header,
-        field_first_section_paragraph,
-        field_second_section_header,
-        field_second_section_paragraph
-    } = contactPageData;
+  if (!contactPageData) {
+    return <p>Loading</p>;
+  }
 
-    type ContactPageField = {
-        value: string;
-        format: string;
-        processed: string;
-    };
+  const {
+    field_first_section_header,
+    field_first_section_paragraph,
+    field_second_section_header,
+    field_second_section_paragraph,
+  } = contactPageData;
 
-    return (
-        <Container fluid className='p-0'>
-            <Container fluid className='p-5 pb-1'>
-                <h1 className='my-3 d-flex gap-2'>
-                    {field_first_section_header.map((span: ContactPageField, index: number) => (
-                        <span key={index} dangerouslySetInnerHTML={{ __html: span.processed }} />
-                    ))}
-                </h1>
-                <p style={{ maxWidth: "500px" }} className='d-flex flex-column gap-3'>
-                    {field_first_section_paragraph.map((span: ContactPageField, index: number) => (
-                        <span key={index} dangerouslySetInnerHTML={{ __html: span.processed }} />
-                    ))}
-                </p>
-            </Container>
-            <Container fluid className='p-5'>
-                <h1 className='my-3 d-flex gap-2'>
-                    {field_second_section_header.map((span: ContactPageField, index: number) => (
-                        <span key={index} dangerouslySetInnerHTML={{ __html: span.processed }} />
-                    ))}
-                </h1>
-                <p style={{ maxWidth: "500px" }} className='d-flex flex-column gap-3'>
-                    {field_second_section_paragraph.map((span: ContactPageField, index: number) => (
-                        <span key={index} dangerouslySetInnerHTML={{ __html: span.processed }} />
-                    ))}
-                </p>
-            </Container>
+  type ContactPageField = {
+    value: string;
+    format: string;
+    processed: string;
+  };
 
-            <ContactPageTeam teamMembersData={teamMembersData} />
+  return (
+    <Container fluid className="p-0">
+      <Container className="py-5 d-flex flex-column">
+        <h1 className="my-3 gap-2">
+          {field_first_section_header.map(
+            (span: ContactPageField, index: number) => (
+              <span
+                key={index}
+                dangerouslySetInnerHTML={{ __html: span.processed }}
+              />
+            )
+          )}
+        </h1>
+        <p
+          style={{ maxWidth: "70%" }}
+          className="lead d-flex flex-column gap-5 px-5"
+        >
+          {field_first_section_paragraph.map(
+            (span: ContactPageField, index: number) => (
+              <span
+                key={index}
+                dangerouslySetInnerHTML={{ __html: span.processed }}
+              />
+            )
+          )}
+        </p>
+      </Container>
+      <Container>
+        <h1 className="my-3 d-flex gap-2">
+          {field_second_section_header.map(
+            (span: ContactPageField, index: number) => (
+              <span
+                key={index}
+                dangerouslySetInnerHTML={{ __html: span.processed }}
+              />
+            )
+          )}
+        </h1>
+        <p
+          style={{ maxWidth: "70%" }}
+          className="lead d-flex flex-column gap-3 px-5"
+        >
+          {field_second_section_paragraph.map(
+            (span: ContactPageField, index: number) => (
+              <span
+                key={index}
+                dangerouslySetInnerHTML={{ __html: span.processed }}
+              />
+            )
+          )}
+        </p>
+      </Container>
 
+      <ContactPageTeam teamMembersData={teamMembersData} />
+
+      <Container fluid className="bg-light">
+        <Row>
+          <Col md={6}>
             <ContactForm />
-        </Container>
-    )
-}
+          </Col>
+          <Col md={6} className="bg-dark text-light py-5">
+            <h3 className="my-5 text-center">
+              Interested in joining our team?
+            </h3>
+
+            <div className="d-flex flex-column  justify-content-center align-items-center text-center gap-4">
+              <div>
+                <span>
+                  <span className="text-danger pe-1">&rarr;</span>Take a look at
+                  our open positions
+                </span>
+              </div>
+              <div>
+                <p>Client support</p>
+                <span>+358 20 187 6601 (Mon to Fri 8-17)</span>
+              </div>
+              <div>
+                <p>Sales</p>
+                <span>+358 20 187 6601 (Mon to Fri 8-17)</span>
+              </div>
+              <div>
+                <p>Office</p>
+                <span>+358 20 187 6601 (Mon to Fri 8-17)</span>
+              </div>
+              <div>
+                <span>
+                  <span className="text-danger pe-1">&rarr;</span> Billing
+                  information
+                </span>
+              </div>
+            </div>
+          </Col>
+        </Row>
+      </Container>
+    </Container>
+  );
+};
 
 export default Contact;
--- Frontend/src/components/ContactForm.tsx
+++ Frontend/src/components/ContactForm.tsx
@@ -4,25 +4,25 @@ import { Form } from "react-bootstrap";
 
 // this ID comes from Mautic form components ID
 const formId = {
-  "Feedback": 8,
-  "Client": 5,
-  "Partner": 6,
+  Feedback: 8,
+  Client: 5,
+  Partner: 6,
   "Job Seeker": 7,
 };
 
 type FormType = keyof typeof formId;
 
 export default function ContactForm() {
-  const [currentFormType, setCurrentFormType] = useState<FormType>('Feedback');
+  const [currentFormType, setCurrentFormType] = useState<FormType>("Feedback");
 
   return (
-    <Container fluid className="py-5 d-flex flex-column align-items-center" style={{ height: '1000px', padding: 0 }}>
-      <h2>Send us a message</h2>
+    <Container fluid className="py-5 d-flex flex-column align-items-center">
+      <h2 className="my-5">Send us a message</h2>
       <Container fluid className="d-flex justify-content-center gap-2">
         {Object.keys(formId).map((type) => (
           <Button
             key={type}
-            variant="outline-primary"
+            variant="outline-danger"
             onClick={() => setCurrentFormType(type as FormType)}
             active={currentFormType === type}
           >
@@ -60,47 +60,57 @@ export default function ContactForm() {
           data-mautic-form="contactformclient"
           encType="multipart/form-data"
         >
-          <div className="mauticform-error" id="mauticform_contactformclient_error"></div>
-          <div className="mauticform-message" id="mauticform_contactformclient_message"></div>
+          <div
+            className="mauticform-error"
+            id="mauticform_contactformclient_error"
+          ></div>
+          <div
+            className="mauticform-message"
+            id="mauticform_contactformclient_message"
+          ></div>
           <div className="mauticform-innerform">
             <Container
               className="mauticform-page-wrapper mauticform-page-1"
               data-mautic-form-page="1"
               style={{ display: "flex", flexDirection: "column", gap: "1rem" }}
             >
-              <Form.Group id="mauticform_contactformclient_first_name" className="mauticform-row mauticform-text mauticform-field-1">
-                <Form.Label
-                  id="mauticform_label_contactformclient_first_name"
-                  htmlFor="mauticform_input_contactformclient_first_name"
-                  className="mauticform-label"
+              <div className="d-flex gap-3">
+                <Form.Group
+                  id="mauticform_contactformclient_first_name"
+                  className="w-50"
                 >
-                  First name
-                </Form.Label>
-                <Form.Control
-                  type="text"
-                  name="mauticform[first_name]"
-                  id="mauticform_input_contactformclient_first_name"
-                  className="mauticform-input"
-                />
-                <span className="mauticform-errormsg" style={{ display: "none" }}></span>
-              </Form.Group>
+                  <Form.Label
+                    htmlFor="mauticform_input_contactformclient_first_name"
+                    className="mauticform-label"
+                  >
+                    First name
+                  </Form.Label>
+                  <Form.Control
+                    type="text"
+                    name="mauticform[first_name]"
+                    id="mauticform_input_contactformclient_first_name"
+                    className="mauticform-input"
+                  />
+                </Form.Group>
 
-              <Form.Group id="mauticform_contactformclient_last_name" className="mauticform-row mauticform-text mauticform-field-2">
-                <Form.Label
-                  id="mauticform_label_contactformclient_last_name"
-                  htmlFor="mauticform_input_contactformclient_last_name"
-                  className="mauticform-label"
+                <Form.Group
+                  id="mauticform_contactformclient_last_name"
+                  className="w-50"
                 >
-                  Last name
-                </Form.Label>
-                <Form.Control
-                  type="text"
-                  name="mauticform[last_name]"
-                  id="mauticform_input_contactformclient_last_name"
-                  className="mauticform-input"
-                />
-                <span className="mauticform-errormsg" style={{ display: "none" }}></span>
-              </Form.Group>
+                  <Form.Label
+                    htmlFor="mauticform_input_contactformclient_last_name"
+                    className="mauticform-label"
+                  >
+                    Last name
+                  </Form.Label>
+                  <Form.Control
+                    type="text"
+                    name="mauticform[last_name]"
+                    id="mauticform_input_contactformclient_last_name"
+                    className="mauticform-input"
+                  />
+                </Form.Group>
+              </div>
 
               <Form.Group
                 id="mauticform_contactformclient_email"
@@ -121,10 +131,18 @@ export default function ContactForm() {
                   id="mauticform_input_contactformclient_email"
                   className="mauticform-input"
                 />
-                <span className="mauticform-errormsg" style={{ display: "none" }}>Wrong format!</span>
+                <span
+                  className="mauticform-errormsg"
+                  style={{ display: "none" }}
+                >
+                  Wrong format!
+                </span>
               </Form.Group>
 
-              <Form.Group id="mauticform_contactformclient_phone_number" className="mauticform-row mauticform-tel mauticform-field-4">
+              <Form.Group
+                id="mauticform_contactformclient_phone_number"
+                className="mauticform-row mauticform-tel mauticform-field-4"
+              >
                 <Form.Label
                   id="mauticform_label_contactformclient_phone_number"
                   htmlFor="mauticform_input_contactformclient_phone_number"
@@ -138,10 +156,16 @@ export default function ContactForm() {
                   id="mauticform_input_contactformclient_phone_number"
                   className="mauticform-input"
                 />
-                <span className="mauticform-errormsg" style={{ display: "none" }}></span>
+                <span
+                  className="mauticform-errormsg"
+                  style={{ display: "none" }}
+                ></span>
               </Form.Group>
 
-              <Form.Group id="mauticform_contactformclient_f_message" className="mauticform-row mauticform-text mauticform-field-5">
+              <Form.Group
+                id="mauticform_contactformclient_f_message"
+                className="mauticform-row mauticform-text mauticform-field-5"
+              >
                 <Form.Label
                   id="mauticform_label_contactformclient_f_message"
                   htmlFor="mauticform_input_contactformclient_f_message"
@@ -150,29 +174,52 @@ export default function ContactForm() {
                   Message
                 </Form.Label>
                 <Form.Control
+                  as="textarea"
+                  rows={4}
                   name="mauticform[f_message]"
                   id="mauticform_input_contactformclient_f_message"
                   className="mauticform-textarea"
                 />
-                <span className="mauticform-errormsg" style={{ display: "none" }}></span>
+                <span
+                  className="mauticform-errormsg"
+                  style={{ display: "none" }}
+                ></span>
               </Form.Group>
 
-              <Form.Group id="mauticform_contactformclient_submit" className="mauticform-row mauticform-button-wrapper mauticform-field-6">
+              <Form.Group
+                id="mauticform_contactformclient_submit"
+                className="mauticform-row mauticform-button-wrapper mauticform-field-6"
+              >
                 <Button
                   className="btn btn-default mauticform-button"
                   name="mauticform[submit]"
                   value="1"
                   id="mauticform_input_contactformclient_submit"
                   type="submit"
+                  variant="danger"
                 >
                   Submit
                 </Button>
               </Form.Group>
             </Container>
           </div>
-          <input type="hidden" name="mauticform[formId]" id="mauticform_contactformclient_id" value={formId[currentFormType]} />
-          <input type="hidden" name="mauticform[return]" id="mauticform_contactformclient_return" />
-          <input type="hidden" name="mauticform[formName]" id="mauticform_contactformclient_name" value="contactformclient" />
+          <input
+            type="hidden"
+            name="mauticform[formId]"
+            id="mauticform_contactformclient_id"
+            value={formId[currentFormType]}
+          />
+          <input
+            type="hidden"
+            name="mauticform[return]"
+            id="mauticform_contactformclient_return"
+          />
+          <input
+            type="hidden"
+            name="mauticform[formName]"
+            id="mauticform_contactformclient_name"
+            value="contactformclient"
+          />
         </Form>
       </Container>
     </Container>
