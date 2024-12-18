4b7d3caebf38f86dbca9c0b8dcc1fc1ce51750c8
Alex Tran <tranduclong092@gmail.com>
2024-12-18 21:45:49 +0700

    SCRUM-159 Feat: retrieve single project data from state

--- Frontend/src/components/ProjectsPage/ProjectDetailPage.tsx
+++ Frontend/src/components/ProjectsPage/ProjectDetailPage.tsx
@@ -1,15 +1,26 @@
 import React from "react";
-import { useParams } from "react-router-dom";
+import { Navigate, useParams } from "react-router-dom";
 import { Container, Row, Col, Image } from "react-bootstrap";
+import { useAppSelector } from "../../hooks/hooks";
 
 const ProjectDetailPage: React.FC = () => {
   const { id } = useParams<{ id: string }>(); // Extract project ID from the route
+  const projectsData = useAppSelector((state) => state.drupal.projectsData);
+  const thisProjectData = projectsData.find((project) => project.drupal_internal__nid == id);
+
+  if (!thisProjectData) {
+    // redirect back to projects page if project not found
+    return Navigate({ to: "/projects" });
+  }
+
+  console.log(thisProjectData);
+
 
   return (
     <Container className="py-5">
       <Row className="mb-4">
         <Col xs={12} md={6}>
-          <h1>Dummy Project Title {id}</h1>
+          <h1></h1>
           <p>
             This is a placeholder description for the project with ID:{" "}
             <strong>{id}</strong>. You can replace this content with actual
